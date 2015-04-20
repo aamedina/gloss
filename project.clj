edd94325d1a4710335b6c8559a9897d57923e980
Adrian Medina <adrian.medina@mail.yu.edu>
2015-04-20 17:53:59 -0400

    init fork

--- project.clj
+++ project.clj
@@ -1,15 +1,12 @@
-(defproject gloss "0.2.5"
+(defproject gloss "0.2.6-SNAPSHOT"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[manifold "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [manifold "0.1.0"]
                  [byte-streams "0.2.0"]
-                 [potemkin "0.3.13"]]
-  :aliases {"all" ["with-profile" "1.6,dev:1.3,dev:dev:1.4,dev"]}
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
+                 [potemkin "0.3.13"]
+                 [io.netty/netty-buffer "4.0.27.Final"]]
+  :repositories {"sonatype" "https://oss.sonatype.org/content/groups/public/"}
   :jvm-opts ["-server"])
